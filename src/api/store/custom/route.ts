f3342faa6040ace5f0ce217ae7b15439cac690ce
Your Name <mhal236@uky.edu>
2025-02-24 20:00:19 -0500

    Your commit message here

--- src/api/store/custom/route.ts
+++ src/api/store/custom/route.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from "@medusajs/framework";
+import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
 
 export async function GET(
-  req: Request,
-  res: Response
+  req: MedusaRequest,
+  res: MedusaResponse
 ): Promise<void> {
   res.sendStatus(200);
 }
